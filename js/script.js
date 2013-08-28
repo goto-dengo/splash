1a680e7b78f9dca1cdfdadfce62448b0348b539b
adamjacobbecker <ad@mbecker.cc>
2013-08-28 09:53:56 -0700

    build using jekyll

---
+++ js/script.js
@@ -0,0 +1,17 @@
+---
+layout: none
+---
+
+{% capture js %}
+{% include js/jquery.form.js %}
+{% include js/jquery.scrolldepth.js %}
+{% include js/foundation.js %}
+{% include js/foundation.topbar.js %}
+{% include js/extensions.js %}
+{% include js/flash_placeholder.js %}
+{% include js/contact_us.js %}
+{% include js/scroll_to.js %}
+{% include js/subscribe_to_mailing_list.js %}
+{% include js/main.js %}
+{% endcapture %}
+{{ js }}
