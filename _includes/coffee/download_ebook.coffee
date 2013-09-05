0c74f6cf48efa5631a56a5102a33ba9f6c091502
adamjacobbecker <ad@mbecker.cc>
2013-09-05 11:13:14 -0700

    add eventAction

--- _includes/coffee/download_ebook.coffee
+++ _includes/coffee/download_ebook.coffee
@@ -4,7 +4,7 @@ $(document).on 'submit', '.download-ebook-form', (e) ->
   downloadUrl = $('.gh-link').attr('href')
   email = $(@).find('input').val()
 
-  ga('send', 'event', 'Download ebook')
+  ga('send', 'event', 'Download ebook', 'clicked')
 
   $.getJSON "http://dobt.us7.list-manage1.com/subscribe/post-json?u=9f7643525159ed54ae9670c8a"+
             "&amp;id=9b82ed8ef1&c=?&EMAIL=#{email}", =>
--- _includes/js/download_ebook.js
+++ _includes/js/download_ebook.js
@@ -7,7 +7,7 @@
     e.preventDefault();
     downloadUrl = $('.gh-link').attr('href');
     email = $(this).find('input').val();
-    ga('send', 'event', 'Download ebook');
+    ga('send', 'event', 'Download ebook', 'clicked');
     return $.getJSON("http://dobt.us7.list-manage1.com/subscribe/post-json?u=9f7643525159ed54ae9670c8a" + ("&amp;id=9b82ed8ef1&c=?&EMAIL=" + email), function() {
       $(_this).hide();
       $(".download-ebook-link").show();
